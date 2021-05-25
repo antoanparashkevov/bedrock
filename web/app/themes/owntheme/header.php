24149a5feac85ee50640e8bb01de36f8c3f1a0e2
Antoan Parashkevov <tonkata1505@gmail.com>
2021-05-25 19:06:56 +0300

    Making Header

--- web/app/themes/owntheme/header.php
+++ web/app/themes/owntheme/header.php
@@ -64,6 +64,66 @@
         </div>
 
 	<header id="masthead" class="site-header">
+
+	<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">zendex</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+<li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+<li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+<li class="nav-item">
+          <a class="nav-link" href="#">Portfolio</a>
+        </li>
+<li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+<li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+</ul>
+</div>
+</div>
+</nav>
+
+
+	<div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel" data-bs-interval="5000">
+	
+  <div class="carousel-inner">
+
+    <div class="carousel-item active">
+	
+      <img class="d-block w-100" src="http://dtaccu.wpengine.com/wp-content/uploads/2018/08/slider1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="http://dtaccu.wpengine.com/wp-content/uploads/2018/08/slider2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="http://dtaccu.wpengine.com/wp-content/uploads/2018/08/slider3.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+	
+
+	
+
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
