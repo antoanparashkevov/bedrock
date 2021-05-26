796d9a9b546d3f738a20cc3e2052949d996643a3
Boris Sanchez | ThinkPad E470 | Debian 9 <sanchobor@gmail.com>
2021-05-26 13:50:33 +0300

    removed fixed-top and added dark color to menu

--- web/app/themes/owntheme/header.php
+++ web/app/themes/owntheme/header.php
@@ -65,34 +65,34 @@
 
 	<header id="masthead" class="site-header">
 
-	<nav class="navbar navbar-expand-lg navbar-light fixed-top">
-  <div class="container">
-    <a class="navbar-brand" href="#">zendex</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ml-auto">
-<li class="nav-item active">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-<li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-<li class="nav-item">
-          <a class="nav-link" href="#">Portfolio</a>
-        </li>
-<li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
-        </li>
-<li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-</ul>
-</div>
-</div>
-</nav>
+	<nav class="navbar navbar-expand-lg navbar-light">
+		<div class="container">
+            <a class="navbar-brand text-dark" href="#">zendex</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+        
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                  <a class="nav-link text-dark" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-dark" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-dark" href="#">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-dark" href="#">Services</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-dark" href="#">Contact</a>
+                </li>
+                </ul>
+        	</div>
+		</div>
+	</nav>
 
 
 	<div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel" data-bs-interval="5000">
